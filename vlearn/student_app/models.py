6f1fecb2e223cf9c6197b1d0d1b1afeb12fa1199
Febin <febinvinod@gmail.com>
2025-01-20 16:44:27 +0530

    student

--- vlearn/student_app/models.py
+++ vlearn/student_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    instructor = models.CharField(max_length=200)
+    thumbnail = models.ImageField(upload_to='course_thumbnails/')
+
+    def __str__(self):
+        return self.title
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    date_enrolled = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student.username} - {self.course.title}"
--- vlearn/student_app/views.py
+++ vlearn/student_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Course, Enrollment
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def browse_courses(request):
+    courses = Course.objects.all()
+    return render(request, 'student_app/browse_courses.html', {'courses': courses})
+
+
+def enroll_course(request, course_id):
+    course = get_object_or_404(Course, id=course_id)
+    enrollment, created = Enrollment.objects.get_or_create(student=request.user, course=course)
+    if created:
+        return redirect('my_courses')
+    return render(request, 'student_app/enroll_error.html', {'course': course})
+
+
+def my_courses(request):
+    enrollments = Enrollment.objects.filter(student=request.user)
+    return render(request, 'student_app/my_courses.html', {'enrollments': enrollments})

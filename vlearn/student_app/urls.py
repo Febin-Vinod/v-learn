4c784bad056e058d136dbe4b4a54566aa7bafd8c
Febin <febinvinod@gmail.com>
2025-01-21 12:29:48 +0530

    msg

--- vlearn/student_app/urls.py
+++ vlearn/student_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import BrowseCoursesView, EnrollCourseView, MyCoursesView
+from . import views
+from .views import BrowseCoursesView, StudentDashboardView, CourseDetailView
 
 urlpatterns = [
     path('browse/', BrowseCoursesView.as_view(), name='browse_courses'),
-    path('enroll/<int:course_id>/', EnrollCourseView.as_view(), name='enroll_course'),
-    path('my_courses/', MyCoursesView.as_view(), name='my_courses'),
+    path('dashboard/', StudentDashboardView.as_view(), name='student_dashboard'),
+    path('course/<int:course_id>/', CourseDetailView.as_view(), name='course_detail'),
+    path('course/<int:course_id>/payment/', views.course_payment, name='course_payment'),
+    path('confirm_payment/', views.confirm_payment, name='confirm_payment'),
+    path('payment/success/<int:course_id>/', views.payment_success, name='payment_success'),
 ]
